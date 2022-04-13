61b96e50490e10201e6e4dcaf36003874a46a09a
Гость <Guest@iMac-Mac005-2.local>
2022-04-13 15:48:22 +0900

    first commit

---
+++ negr/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  negr
+//
+//  Created by Гость on 13.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var negr1: UIImageView!
+    @IBOutlet weak var negr2: UIImageView!
+    @IBOutlet weak var negr6: UIImageView!
+    @IBOutlet weak var negr7: UILabel!
+    @IBOutlet weak var negr67: UIImageView!
+    
+    var stateSemafor: Int = 1
+    var timerGame: Timer!
+    var timerPC: Timer!
+
+}
+
+@IBOutlet weak var startGame(_ sender: UIButton) {
+    semaforLabel.isHidden = false
+    
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
